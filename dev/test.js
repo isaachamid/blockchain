6e2facd222cffb5362a806d05bd84e0c7b9934ed
hamidisaac <isaac.hamid@gmail.com>
2024-11-06 12:34:16 +0330

    test new transaction

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,14 @@ const Blockchain = require("./blockchain");
 
 const mycoin = new Blockchain();
 mycoin.createNewBlock(111, "AAA", "BBB");
+
+mycoin.createNewTransaction(100, "SENDER_ADDRESS", "RECIPIENT_ADDRESS");
+
 mycoin.createNewBlock(112, "CCC", "DDD");
+
+mycoin.createNewTransaction(30, "SENDER_ADDRESS", "RECIPIENT_ADDRESS");
+mycoin.createNewTransaction(85, "SENDER_ADDRESS", "RECIPIENT_ADDRESS");
+mycoin.createNewTransaction(1300, "SENDER_ADDRESS", "RECIPIENT_ADDRESS");
 mycoin.createNewBlock(113, "EEE", "FFF");
+
 console.log(mycoin);
